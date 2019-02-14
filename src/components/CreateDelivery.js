c18dd57d692024e4265b643ec24f8fe429394a0b
Jean Loureiro <jean_loureiro@hotmail.com>
2019-02-14 15:44:54 +1000

    [add] form to create page

--- src/components/CreateDelivery.js
+++ src/components/CreateDelivery.js
@@ -5,7 +5,33 @@ class CreateDelivery extends Component {
     render() {
         return (
             <Fragment>
-                <h1>Create Delivery</h1>
+                <main role="main">
+                    <h1>Create Delivery</h1>
+                    <form action="create.php" method="POST">
+                        <div className="form-group row">
+                            <label for="deliveryDate" className="col-sm-2 col-form-label">Date</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="deliveryDate" name="date" value="" />
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label for="deliveryName" className="col-sm-2 col-form-label">Name</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="deliveryName" name="name" value="" />
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label for="deliveryDriver" className="col-sm-2 col-form-label">Driver</label>
+                            <div className="col-sm-10">
+                                <select className="form-control" id="deliveryDriver" name="driver_id">
+                                    <option value="">- Select One -</option>
+                                    <option value="1" >Driver 1</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Create</button>
+                    </form>
+                </main>
             </Fragment>
         )
     }
