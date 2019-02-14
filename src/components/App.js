d26bf1cc8192eae9c5ffa0a56786b8431b317081
Jean Loureiro <jean_loureiro@hotmail.com>
2019-02-14 15:38:54 +1000

    [add] Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
-import Header from './Header'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import DeliveriesList from './DeliveriesList'
+import CreateDelivery from './CreateDelivery'
 
 import '../css/App.css'
+
 
 
 class App extends Component {
     render() {
         return (
             <div className='container'>
-                <Header />
-                <DeliveriesList />
-            </div>    
+                <Router>
+                    <header className="header clearfix">
+
+                        <nav>
+                            <ul className="nav nav-pills float-right">
+                                <li className="nav-item">
+                                    <Link to="/" className="nav-link active">Deliveries</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/create" className="nav-link">New Delivery</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <h3 className="text-muted">CartonCloud</h3>
+
+                        <Route exact path="/" component={DeliveriesList} />
+                        <Route path="/create" component={CreateDelivery} />
+
+                    </header>
+                </Router>
+
+            </div>
         )
     }
 }
---
+++ src/components/CreateDelivery.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+
+class CreateDelivery extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <h1>Create Delivery</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default CreateDelivery
